3c5a0a2f44eafbe35f525f6625cd9e8b94e7537f
archanatimilsina <archanatimilsina88@gmail.com>
2024-02-20 21:32:12 +0545

    update feedback page

--- feedback.php
+++ feedback.php
@@ -1,5 +1,40 @@
+
+<?php
+include('inc/header.php');
+include('connection.php');
+?>
+
+<section id="feedback">
+</section>
+<section id="feedback-form">
+    <p class="feedback_head">Write Your Feedback</p>
+    <form action="feedback.php" method="post">
+       <input type="text" name="name" placeholder="Enter Name">
+       <input type="text" name="feedback" placeholder="Enter Feedback">
+       <input type="submit" name="submit">
+    </form>
+    <?php
+       if(isset($_POST['submit']))
+       {
+           $name = $_POST['name'];
+           $feedback=$_POST['feedback'];
+
+           $qry="INSERT INTO `feedback`(`id`, `name`, `feedback`) VALUES ('','$name','$feedback')";
+           $run=mysqli_query($sql,$qry);
+           if($run==true)
+           {
+               ?>
+               <script>
+                   alert("Feedback Save Perfectly");
+               </script>
+               <?php
+           }
+       }
+    ?>
+</section>
+
+
 <?php 
-include("inc/header.php");
 include("inc/script.php");
 include("inc/footertag.php");
 ?>

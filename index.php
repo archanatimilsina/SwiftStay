48b9a4a93e268da641f8fc707ec7b828db93e49b
Dikshya679 <dikshyashrestha679@gmail.com>
2024-04-22 23:49:25 +0545

    added apr 22

--- index.php
+++ index.php
@@ -2,8 +2,88 @@
 <link rel="stylesheet" href="css/index.css">
 <?php require('inc/navbar.php');?>
 
+  <!-- ----------  Home --------------------- -->
+  <section id="home">
+        <h1 class="h1">Welcome to SwiftStay</h1>
+    </section>
 
-   
+<section id="rooms-right">
+        <div class="paras">
+          <p class="sectionTag">A.C Delux Room</p>
+          <p class="sectionsubTag font">We have the best services provider in Room Management. with 24 hours room services and 24 hours check-out. We provide world class services in very low cost. i.e. 1100rs.</p>
+          <p class="price">Price per room : 1100Rs/-</p>
+          <a href="room.php"><button class="price-btn" >Book A Room</button></a>
+          </div>
+          <div class="thumbnail">
+          <img src="img/deluxe.jpg" alt="delux" class="imgFluid">
+          </div>
+</section>
+<section id="rooms-left">
+        <div class="paras">
+          <p class="sectionTag">A.C. Room</p>
+          <p class="sectionsubTag font">We have the best services provider in Room Management. with 24 hours room services and 24 hours check-out. We provide world class services in very low cost. i.e. 900rs.</p>
+          <p class="price">Price per room : 900Rs/-</p>
+          <a href="room.php"><button class="price-btn">Book A Room</button></a>
+          </div>
+          <div class="thumbnail">
+           <img src="img/ac.jpg" alt="delux" class="imgFluidd">
+          </div>
+    </section>
+
+    <section id="rooms-right">
+        <div class="paras">
+          <p class="sectionTag">Non A.C. Room</p>
+          <p class="sectionsubTag font">We have the best services provider in Room Management. with 24 hours room services and 24 hours check-out. We provide world class services in very low cost. i.e. 700rs.</p>
+          <p class="price">Price per room : 700Rs/-</p>
+            <a href="room.php"><button class="price-btn">Book A Room</button></a>
+          </div>
+          <div class="thumbnail">
+          <img src="img/non.jpg" alt="delux" class="imgFluid">
+          </div>
+    </section>
+
+
+ <!-- -------------------------food ------------------------ -->
+ <section id="services-container">
+        <h1 class="h-primary center">Our Speciality</h1>
+        <div id="services">
+            <div class="box">
+                <a href="food.php"><img src="img/thali.jpg" alt="manchurion"></a>
+               <h2 class="h-secondary center">Typical Nepali Thali</h2>
+               <p class="center">The Nepali staples consist primarily of rice, wheat, corn and lentils, in addition to fresh vegetables and meats. A typical Nepali everyday meal can be characterized by Dal (lentil soups), Bhat (steamed rice), and Tarkari (vegetable preparations), also known as “The Trinity,” supplemented by some meat preparation.
+               </p>
+            </div>
+            <div class="box">
+                 <a href="food.php"><img src="img/momos.jpg" alt="pasta"></a>
+                <h2 class="h-secondary center">Momos</h2>
+                <p class="center"> Momos are a type of steamed filled dumpling in Tibetan and Nepali cuisine that is also popular in neighbouring Bhutan and India. Momos are usually served with a sauce known as achar influenced by the spices and herbs used within many South Asian cuisines.</p>
+             </div>
+             <div class="box">
+             <a href="food.php"><img src="img/burger.jpg" alt="maharshtrian"></a>
+                <h2 class="h-secondary center">Burger</h2>
+                <p class="center">Some common types of burgers include beef burgers, chicken burgers, lamb burgers, fish burgers, veggie burgers, and black bean burgers. Within these categories, there are numerous variations that can be made by using different types of meats, vegetables, and seasonings</p>
+             </div>
+        </div>
+        <div id="services">
+            <div class="box">
+            <a href="food.php"><img src="img/drink.jpg" alt="panner"></a>
+               <h2 class="h-secondary center">Drinks</h2>
+               <p class="center">A drink is a liquid intended for consumption. The function of satisfying thirst, drinks play role in human culture. Common types of drinks include plain drinking water, milk, juice, smoothies and soft drinks. Warm beverages include coffee, tea, and hot chocolate.
+               </p>
+            </div>
+            <div class="box">
+            <a href="food.php"><img src="img/cake.jpg" alt="dosa"></a>
+                <h2 class="h-secondary center">Dessert</h2>
+                <p class="center">The term dessert can apply to many sweets, such as biscuits, cakes, cookies, custards, gelatins, ice creams, pastries, pies, puddings, macaroons, sweet soups, tarts, and fruit salad (fruit is commonly found in dessert courses because of its naturally occurring sweetness)</p>
+             </div>
+             <div class="box">
+             <a href="food.php"><img src="img/spa.jpg" alt="faluda"></a>
+                <h2 class="h-secondary center">Snacks</h2>
+                <p class="center">A small amount of food that is eaten between meals, or a very small meal: I had a huge lunch, so I'll only need a snack for dinner. Fresh or dried fruit makes an ideal snack. Many snack foods are high in salt, sugar, and fat.</p>
+             </div>
+        </div>
+       
+    </section>
 
 
     

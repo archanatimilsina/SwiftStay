e0ab32f9bccffdc35aac489f80d86278bdccfe85
Dikshya679 <dikshyashrestha679@gmail.com>
2024-04-22 20:53:20 +0545

    pushed

--- index.php
+++ index.php
@@ -3,92 +3,10 @@
 <?php require('inc/navbar.php');?>
 
 
-    <!-- ----------  Home --------------------- -->
-    <section id="home">
-        <h1 class="h1">Welcome to SwiftStay</h1>
-    </section>
+   
 
 
-    <section id="rooms-right">
-        <div class="paras">
-          <p class="sectionTag">A.C Delux Room</p>
-          <p class="sectionsubTag font">We have the best services provider in Room Management. with 24 hours room services and 24 hours check-out. We provide world class services in very low cost. i.e. 1100rs.</p>
-          <p class="price">Price per room : 1100Rs/-</p>
-          <a href="room.php"><button class="price-btn" >Book A Room</button></a>
-          </div>
-          <div class="thumbnail">
-          <img src="img/deluxe.jpg" alt="delux" class="imgFluid">
-          </div>
-    </section>
-
-    <section id="rooms-left">
-        <div class="paras">
-          <p class="sectionTag">A.C. Room</p>
-          <p class="sectionsubTag font">We have the best services provider in Room Management. with 24 hours room services and 24 hours check-out. We provide world class services in very low cost. i.e. 900rs.</p>
-          <p class="price">Price per room : 900Rs/-</p>
-          <a href="room.php"><button class="price-btn">Book A Room</button></a>
-          </div>
-          <div class="thumbnail">
-           <img src="img/ac.jpg" alt="delux" class="imgFluidd">
-          </div>
-    </section>
-
-    <section id="rooms-right">
-        <div class="paras">
-          <p class="sectionTag">Non A.C. Room</p>
-          <p class="sectionsubTag font">We have the best services provider in Room Management. with 24 hours room services and 24 hours check-out. We provide world class services in very low cost. i.e. 700rs.</p>
-          <p class="price">Price per room : 700Rs/-</p>
-            <a href="room.php"><button class="price-btn">Book A Room</button></a>
-          </div>
-          <div class="thumbnail">
-          <img src="img/non.jpg" alt="delux" class="imgFluid">
-          </div>
-    </section>
-
-     <!-- -------------------------food ------------------------ -->
-     <section id="services-container">
-        <h1 class="h-primary center">Our Speciality</h1>
-        <div id="services">
-            <div class="box">
-                <a href="food.php"><img src="img/thali.jpg" alt="manchurion"></a>
-               <h2 class="h-secondary center">Typical Nepali Thali</h2>
-               <p class="center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, assumenda magni sequi maxime ullam magnam minima porro tempore enim sed quidem sint provident facere fugiat facilis qui saepe quia? Aut, repudiandae?
-                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. amet consectetur adipisicing elit. Quas nulla sit, perspiciatis temporibus unde
-               </p>
-            </div>
-            <div class="box">
-                 <a href="food.php"><img src="img/momos.jpg" alt="pasta"></a>
-                <h2 class="h-secondary center">Momos</h2>
-                <p class="center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, assumenda magni sequi maxime ullam magnam minima porro tempore enim sed quidem sint provident facere fugiat facilis qui saepe quia? Aut, repudiandae?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nulla sit, perspiciatis temporibus unde </p>
-             </div>
-             <div class="box">
-             <a href="food.php"><img src="img/burger.jpg" alt="maharshtrian"></a>
-                <h2 class="h-secondary center">Burger</h2>
-                <p class="center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, assumenda magni sequi maxime ullam magnam minima porro tempore enim sed quidem sint provident Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure mollitia aliquid labore illum ipsa aliquam ad, commodi voluptatum libero minima nesciunt autem quis minus nostrum.</p>
-             </div>
-        </div>
-        <div id="services">
-            <div class="box">
-            <a href="food.php"><img src="img/drink.jpg" alt="panner"></a>
-               <h2 class="h-secondary center">Drinks</h2>
-               <p class="center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, assumenda magni sequi maxime ullam magnam minima porro tempore enim sed quidem sint provident facere fugiat facilis qui saepe quia? Aut, repudiandae?
-                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis saepe error modi quo vel!
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-               </p>
-            </div>
-            <div class="box">
-            <a href="food.php"><img src="img/cake.jpg" alt="dosa"></a>
-                <h2 class="h-secondary center">Dessert</h2>
-                <p class="center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, assumenda magni sequi maxime ullam magnam minima porro tempore enim sed quidem sint provident facere fugiat facilis qui saepe quia? Aut, repudiandae?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nulla sit, perspiciatis temporibus unde </p>
-             </div>
-             <div class="box">
-             <a href="food.php"><img src="img/spa.jpg" alt="faluda"></a>
-                <h2 class="h-secondary center">Snacks</h2>
-                <p class="center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, assumenda magni sequi maxime ullam magnam minima porro tempore enim sed quidem sint provident Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure mollitia aliquid labore illum ipsa aliquam ad, commodi voluptatum libero minima nesciunt autem quis minus nostrum.</p>
-             </div>
-        </div>
-       
-    </section>
+    
 
     <section id="booking-hall">
         <h1 class="h1">Party And Marriage Halls</h1>
